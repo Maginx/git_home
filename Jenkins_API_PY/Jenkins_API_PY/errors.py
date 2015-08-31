f48129a6a50c56a9e8b0ec2e4e8fd6e9bfe41532
Magnix <982397072@qq.com>
2015-08-31 15:52:45 +0800

    update exception by jeremy

--- Jenkins_API_PY/Jenkins_API_PY/errors.py
+++ Jenkins_API_PY/Jenkins_API_PY/errors.py
@@ -1,12 +1,4 @@
-##
-#  @file errors.py
-#  @brief : Define exception for ci modules
-#  @author: Li, Joe (NSN - CN/Cheng Du)
-#  @created on: 2014-01-01
-#  @version: 1.0
-#
-
-class SvnException(Exception): pass
+﻿class SvnException(Exception): pass
 
 class ShellException(Exception): pass
 
